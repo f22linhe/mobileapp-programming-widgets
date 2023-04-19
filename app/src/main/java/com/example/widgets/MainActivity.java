2d3952fad08be0717073c6841c75a80b8ba78169
Lina H <f22linhe@student.his.se>
2023-04-19 19:32:20 +0200

    Lade till kod för att knappen ska fungera.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
+
 
 import android.os.Bundle;
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.My_Button_Widget);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","We clicked on My_Button_Widget!");
+            }
+        });
     }
 }
